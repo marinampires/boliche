885292714c58c2ee92d8b4d5e41ffe9ea778037b
Marina Melo <mmelo@globosat.com.br>
2012-08-23 19:17:53 -0300

    Acrescentando um teste com erro e corrigido o problema do calculo do spare

--- boliche.py
+++ boliche.py
@@ -1,19 +1,22 @@
 def calcula(jogada):
-    pontuacao = []
+    pontuacao = 0
     ponto_anterior = 0
     ponto_mais_que_anterior = 0
+    num_jogada = 1
 
     for ponto in jogada:
         if(ponto == "/"):
             pontuacao += 10 - int(ponto_anterior)
         elif(ponto == "X"):
+            num_jogada += 1
             pontuacao += 10
         elif(ponto != "-"):
             pontuacao += int(ponto)
-            if(ponto_anterior == "X" or ponto_mais_que_anterior == "X"):
+            if(ponto_anterior == "X" or ponto_mais_que_anterior == "X" or ponto_anterior == "/" and jogada and num_jogada < 20):
                 pontuacao += int(ponto)
         else:
             ponto = 0
         ponto_mais_que_anterior = ponto_anterior
         ponto_anterior = ponto
+        num_jogada += 1
     return pontuacao
--- tests.py
+++ tests.py
@@ -5,32 +5,38 @@ from boliche import *
 class TestBoliche(unittest.TestCase):
 
     def test_calcula_jogada_sem_erro_strike_spare(self):
-        jogada = "12345462"
-        esperado = 27
+        jogada = "12345462233223322332"
+        esperado = 57
         calculado = calcula(jogada)
         self.assertEquals(esperado, calculado)
 
     def test_calcula_jogada_com_erro_sem_strike_spare(self):
-        jogada = "2453--"
-        esperado = 14
+        jogada = "9-9-9-9-9-9-9-9-9-9-"
+        esperado = 90
         calculado = calcula(jogada)
         self.assertEquals(esperado, calculado)
 
     def test_calcula_jogada_sem_erro_sem_strike_com_um_spare(self):
-        jogada = "14527/1"
-        esperado = 23
+        jogada = "5/5/5/5/5/5/5/5/5/5/5"
+        esperado = 150
         calculado = calcula(jogada)
         self.assertEquals(esperado, calculado)
 
     def test_calcula_jogada_com_erro_sem_strike_com_spare(self):
-        jogada = "5472-/7"
-        esperado = 35
+        jogada = "547223322332233223-/7"
+        esperado = 70
         calculado = calcula(jogada)
         self.assertEquals(esperado, calculado)
 
     def test_calcula_jogada_sem_erro_com_strike_sem_spare(self):
-        jogada = "81X2526"
-        esperado = 41
+        jogada = "81X2526233223322332"
+        esperado = 71
+        calculado = calcula(jogada)
+        self.assertEquals(esperado, calculado)
+
+    def test_calcula_jogada_somente_com_strike(self):
+        jogada = "XXXXXXXXXXXX"
+        esperado = 300
         calculado = calcula(jogada)
         self.assertEquals(esperado, calculado)
 
