9ecc8d6cfb65ee27b390d49fb807f2398139f276
Marina Melo <mmelo@globosat.com.br>
2012-08-23 17:58:17 -0300

    solução feita pela Bruna, Marina e Victor

--- boliche.py
+++ boliche.py
@@ -1,5 +1,19 @@
 def calcula(jogada):
-    pontos = 0
-    for p in jogada:
-        pontos = pontos + int(p)
-    return pontos
+    pontuacao = []
+    ponto_anterior = 0
+    ponto_mais_que_anterior = 0
+
+    for ponto in jogada:
+        if(ponto == "/"):
+            pontuacao += 10 - int(ponto_anterior)
+        elif(ponto == "X"):
+            pontuacao += 10
+        elif(ponto != "-"):
+            pontuacao += int(ponto)
+            if(ponto_anterior == "X" or ponto_mais_que_anterior == "X"):
+                pontuacao += int(ponto)
+        else:
+            ponto = 0
+        ponto_mais_que_anterior = ponto_anterior
+        ponto_anterior = ponto
+    return pontuacao
--- tests.py
+++ tests.py
@@ -3,14 +3,36 @@ from boliche import *
 
 
 class TestBoliche(unittest.TestCase):
-    def test_deve_acertar_todas_jogadas_sem_spare_ou_strike(self):
-        jogada = ""
-        esperado = 0
-        for i in range(1, 11):
-            jogada = jogada + "22"
-            esperado = esperado + 4
-        retorno = calcula(jogada)
-        self.assertEqual(esperado, retorno)
+
+    def test_calcula_jogada_sem_erro_strike_spare(self):
+        jogada = "12345462"
+        esperado = 27
+        calculado = calcula(jogada)
+        self.assertEquals(esperado, calculado)
+
+    def test_calcula_jogada_com_erro_sem_strike_spare(self):
+        jogada = "2453--"
+        esperado = 14
+        calculado = calcula(jogada)
+        self.assertEquals(esperado, calculado)
+
+    def test_calcula_jogada_sem_erro_sem_strike_com_um_spare(self):
+        jogada = "14527/1"
+        esperado = 23
+        calculado = calcula(jogada)
+        self.assertEquals(esperado, calculado)
+
+    def test_calcula_jogada_com_erro_sem_strike_com_spare(self):
+        jogada = "5472-/7"
+        esperado = 35
+        calculado = calcula(jogada)
+        self.assertEquals(esperado, calculado)
+
+    def test_calcula_jogada_sem_erro_com_strike_sem_spare(self):
+        jogada = "81X2526"
+        esperado = 41
+        calculado = calcula(jogada)
+        self.assertEquals(esperado, calculado)
 
 if __name__ == '__main__':
     unittest.main()
